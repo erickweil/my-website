633560f821c4a914d6f76a3abaae14acf2cfc8e6
Erick Weil <erick.weil@ifro.edu.br>
2026-05-01 14:38:30 -0400

    Configurado build e deploy na vercel via Github Workflow customizado

---
+++ buildWasm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+cd rust-wasm
+# Install wasm-pack if it's not already installed
+if ! command -v wasm-pack &> /dev/null; then
+    echo "wasm-pack could not be found, installing..."
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+fi
+wasm-pack build --release --target web --out-dir ../src/pkg
